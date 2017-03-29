ee6fe9a895bc102133d6da1d361c6ed1661c3326
bitspill <bitspill+github@bitspill.net>
2017-03-29 14:40:47 -0500

    Fix typo
    html and body tags are also html

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
   <body>
     <h1>spee.ch</h1>
-    <p>In just a few hours, this site will morph from this utterly bare 5 lines of HTML into a decentralized, censorship-resistant, truly free image sharing site powered by <a href="https://lbry.io">LBRY</a>.
+    <p>In just a few hours, this site will morph from this utterly bare 9 lines of HTML into a decentralized, censorship-resistant, truly free image sharing site powered by <a href="https://lbry.io">LBRY</a>.
     <p>You can watch it happen in real time, starting at 6pm EST tonight (March 29th):</p> 
     <iframe width="560" height="315" src="https://www.youtube.com/embed/C9LCapt_OYw" frameborder="0" allowfullscreen></iframe>
     <p><a href="https://github.com/lbryio/spee.ch">GitHub repo</a></p> 
