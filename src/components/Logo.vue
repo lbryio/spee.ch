9a3d3c99ca2c9c0e3891936e0f0f5a228cf63219
Rob PW <me@rob.pw>
2017-04-16 14:38:46 +0100

    Replace styles with logo's

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,73 +1,11 @@
 <template>
-  <img src="https://spee.ch/speechlogo" alt="spee.ch logo" style="max-height: 256px; vertical-align: middle; max-width: 256px;" />
+  <img class="logo" src="https://spee.ch/speechlogo" alt="spee.ch logo" />
 </template>
 
 <style>
-.VueToNuxtLogo
-{
-  display: inline-block;
-  animation: turn 2s linear forwards 1s;
-  transform: rotateX(180deg);
-  position: relative;
-  overflow: hidden;
-  height: 180px;
-  width: 245px;
-}
-.Triangle
-{
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 0;
-  height: 0;
-}
-.Triangle--one
-{
-  border-left: 105px solid transparent;
-  border-right: 105px solid transparent;
-  border-bottom: 180px solid #41B883;
-}
-.Triangle--two
-{
-  top: 30px;
-  left: 35px;
-  animation: goright 0.5s linear forwards 3.5s;
-  border-left: 87.5px solid transparent;
-  border-right: 87.5px solid transparent;
-  border-bottom: 150px solid #3B8070;
-}
-.Triangle--three
-{
-  top: 60px;
-  left: 35px;
-  animation: goright 0.5s linear forwards 3.5s;
-  border-left: 70px solid transparent;
-  border-right: 70px solid transparent;
-  border-bottom: 120px solid #35495E;
-}
-.Triangle--four
-{
-  top: 120px;
-  left: 70px;
-  animation: godown 0.5s linear forwards 3s;
-  border-left: 35px solid transparent;
-  border-right: 35px solid transparent;
-  border-bottom: 60px solid #fff;
-}
-
-@keyframes turn {
-  100% {
-    transform: rotateX(0deg);
+  .logo {
+    max-height: 256px;
+    max-width: 256px;
+    vertical-align: middle; 
   }
-}
-@keyframes godown {
-  100% {
-    top: 180px;
-  }
-}
-@keyframes goright {
-  100% {
-    left: 70px;
-  }
-}
 </style>
