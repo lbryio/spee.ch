33dc17b9588afd11827fe1f03f64d7df06407b75
Rob PW <me@rob.pw>
2017-04-19 02:57:40 +0100

    Fix buttons styling

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -5,8 +5,7 @@
 </template>
 
 <style>
-html
-{
+html {
   font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
@@ -16,38 +15,32 @@ html
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
 }
-*, *:before, *:after
-{
+
+*, *:before, *:after {
   box-sizing: border-box;
   margin: 0;
 }
-.button--green
-{
+
+.button {
   display: inline-block;
   border-radius: 4px;
   border: 1px solid #3b8070;
   color: #3b8070;
   text-decoration: none;
   padding: 10px 30px;
-}
-.button--green:hover
-{
-  color: #fff;
-  background-color: #3b8070;
-}
-.button--grey
-{
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-.button--grey:hover
-{
-  color: #fff;
-  background-color: #35495e;
+
+  &.green:hover {
+    color: #fff;
+    background-color: #3b8070;
+  }
+
+  &.grey {
+    margin-left: 15px;
+
+    &:hover {
+      color: #fff;
+      background-color: #35495e;
+    }
+  }
 }
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -11,8 +11,8 @@
       <publish/>
       <explore/>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">Github</a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button grey">Github</a>
       </div>
     </div>
   </section>
@@ -33,33 +33,32 @@ export default {
 </script>
 
 <style>
-.container
-{
+.container {
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-.subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links
-{
-  padding-top: 15px;
+
+  .title {
+    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
+
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
+
+  .links {
+    padding-top: 15px;
+  }
 }
 </style>
