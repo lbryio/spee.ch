44f9df17664b2fefab617d8e75c6d5029b129554
bill bittner <bittner.w@gmail.com>
2017-06-13 11:21:42 -0700

    made client analytics ID a string'

--- helpers/socketHelpers.js
+++ helpers/socketHelpers.js
@@ -47,19 +47,19 @@ module.exports = {
 	publish: function(name, filepath, license, nsfw, socket) {
 		// update the client
 		socket.emit("publish-status", "Your image is being published (this might take a second)...");
-		visitor.event("Publish Route", "Publish Request", name, {filepath, license, nsfw}).send();
+		visitor.event("Publish Route", "Publish Request", name, filepath).send();
 		// create the publish object
 		var publishParams = createPublishParams(name, filepath, license, nsfw);
 		// get a promise to publish
 		lbryApi.publishClaim(publishParams)
 		.then(function(data){
-			visitor.event("Publish", "Publish Success", name, {filepath, license, nsfw}).send();
+			visitor.event("Publish", "Publish Success", name, filepath).send();
 			console.log("publish promise success. Tx info:", data)
 			socket.emit("publish-complete", {name: name, result: data.result});
 			deleteTemporaryFile(filepath);
 		})
 		.catch(function(error){
-			visitor.event("Publish Route", "Publish Failure", name, {filepath, license, nsfw}).send();
+			visitor.event("Publish Route", "Publish Failure", name, filepath).send();
 			console.log("error:", error);
 			socket.emit("publish-failure", handlePublishError(error));
 			deleteTemporaryFile(filepath);
