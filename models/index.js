61d8431d38719860398c328a52ddbc62039cbf89
bill bittner <bittner.w@gmail.com>
2017-06-19 13:22:06 -0700

    updated sequelize logging

--- models/index.js
+++ models/index.js
@@ -4,17 +4,20 @@ const Sequelize = require('sequelize');
 const basename = path.basename(module.filename);
 const config = require('config');
 const db = {};
+const logger = require('winston');
 
 const connectionUri = config.get('Database.MySqlConnectionUri');
-const sequelize = new Sequelize(connectionUri);
+const sequelize = new Sequelize(connectionUri, {
+  logging: false,
+});
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log('Connection has been established successfully.');
+    logger.info('Sequelize has has been established mysql connection successfully.');
   })
   .catch(err => {
-    console.error('Unable to connect to the database:', err);
+    logger.error('Sequelize was nable to connect to the database:', err);
   });
 
 fs.readdirSync(__dirname).filter(file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js').forEach(file => {
