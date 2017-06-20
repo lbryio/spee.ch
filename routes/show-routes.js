54d92a2c083524fd33cc3dfd479b302bd6e85cdd
bill bittner <bittner.w@gmail.com>
2017-06-19 22:37:36 -0700

    basic meme builder in place

--- routes/show-routes.js
+++ routes/show-routes.js
@@ -4,6 +4,11 @@ const logger = require('winston');
 
 module.exports = (app, ua, googleAnalyticsId) => {
   // route to fetch all free public claims
+  app.get('/meme-fodder', ({ originalUrl }, res) => {
+    logger.debug(`GET request on ${originalUrl}`);
+    res.status(200).render('memeFodder');
+  });
+  // route to fetch all free public claims
   app.get('/:name/all', ({ originalUrl, params }, res) => {
     logger.debug(`GET request on ${originalUrl}`);
     // google analytics
