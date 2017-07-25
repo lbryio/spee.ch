4421bf2dc01cd0aec1de123b48b4260674159a2b
bill bittner <bittner.w@gmail.com>
2017-07-24 17:04:46 -0700

    removed unused handlebars helper

--- speech.js
+++ speech.js
@@ -72,16 +72,6 @@ const hbs = expressHandlebars.create({
           return options.inverse(this);
       }
     },
-    twitterCard (type) {
-      return new Handlebars.SafeString(`
-      <meta name="twitter:card" content="summary_large_image">
-      <meta name="twitter:site" content="@spee.ch">
-      <meta name="twitter:creator" content="@SarahMaslinNir">
-      <meta name="twitter:title" content="Parade of Fans for Houston’s Funeral">
-      <meta name="twitter:description" content="NEWARK - The guest list and parade of limousines with celebrities emerging from them seemed more suited to a red carpet event in Hollywood or New York than than a gritty stretch of Sussex Avenue near the former site of the James M. Baxter Terrace public housing project here.">
-      <meta name="twitter:image" content="http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg">
-      `);
-    },
   },
 });
 app.engine('handlebars', hbs.engine);
