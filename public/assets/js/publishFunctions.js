6aff2a5a0aeb81b3d676842187e1051fe1e87e98
bill bittner <bittner.w@gmail.com>
2017-07-25 18:49:02 -0700

    updated file limit 10mb for images

--- public/assets/js/publishFunctions.js
+++ public/assets/js/publishFunctions.js
@@ -12,13 +12,13 @@ function validateFile(file) {
 		case 'image/jpeg':
 		case 'image/png':
 		case 'image/gif':
-			if (file.size > 5000000){
-				throw new Error('Sorry, images are limitted to 5 megabytes.');
+			if (file.size > 10000000){
+				throw new Error('Sorry, images are limited to 10 megabytes.');
 			}
 			break;
 		case 'video/mp4':
 			if (file.size > 50000000){
-				throw new Error('Sorry, videos are limitted to 50 megabytes.');
+				throw new Error('Sorry, videos are limited to 50 megabytes.');
 			}
 			break;
 		default:
