1d08a0eec60de18604b81bd939aeff7bf2d83d8e
bill bittner <bittner.w@gmail.com>
2017-07-28 21:06:33 -0700

    updated license drop down and comparison

--- helpers/functions/isFreePublicClaim.js
+++ helpers/functions/isFreePublicClaim.js
@@ -1,11 +1,9 @@
 const logger = require('winston');
 const licenses = ['Creative Commons', 'Public Domain', 'CC Attribution-NonCommercial 4.0 International'];
+
 module.exports = ({ value }) => {
   logger.debug('checking isFreePublicClaim ?');
-  if (
-    (Array.asList(licenses).contains(value.stream.metadata.license)) &&
-    (!value.stream.metadata.fee || value.stream.metadata.fee.amount === 0)
-  ) {
+  if ((licenses.includes(value.stream.metadata.license)) && (!value.stream.metadata.fee || value.stream.metadata.fee.amount === 0)) {
     return true;
   } else {
     return false;
