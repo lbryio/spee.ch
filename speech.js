39530c4b284d53f79b6147f9f2304f8522fd260d
bill bittner <bittner.w@gmail.com>
2017-08-08 12:29:22 -0700

    removed twitter:player:stream

--- speech.js
+++ speech.js
@@ -91,8 +91,7 @@ const hbs = expressHandlebars.create({
           <meta name="twitter:player:width" content="600" >
           <meta name="twitter:text:player_width" content="600" >
           <meta name="twitter:player:height" content="315" >
-          <meta name="twitter:player:stream" content="${directFileUrl}" >
-          
+   
           `
         );
       } else {
