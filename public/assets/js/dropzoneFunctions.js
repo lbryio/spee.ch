45129f8936960cead5e7609b27b51ef5a7398bbf
bill bittner <bittner.w@gmail.com>
2017-10-11 12:15:38 -0700

    fixed double-file-choosers bug

---
+++ public/assets/js/dropzoneFunctions.js
@@ -0,0 +1,40 @@
+function showInstructions () {
+    document.getElementById('preview-dropzone-instructions').hidden = false;
+    document.getElementById('asset-preview').style.opacity = 0.3;
+}
+
+function hideInstructions () {
+    document.getElementById('preview-dropzone-instructions').hidden = true;
+    document.getElementById('asset-preview').style.opacity = 1;
+}
+
+function triggerFileChooser(fileInputId, event) {
+    document.getElementById(fileInputId).click();
+}
+
+function drop_handler(event) {
+    event.preventDefault();
+    // if dropped items aren't files, reject them
+    var dt = event.dataTransfer;
+    if (dt.items) {
+        if (dt.items[0].kind == 'file') {
+            var droppedFile = dt.items[0].getAsFile();
+            previewAndStageFile(droppedFile);
+        }
+    }
+}
+
+function dragover_handler(event) {
+    event.preventDefault();
+}
+
+function dragend_handler(event) {
+    var dt = event.dataTransfer;
+    if (dt.items) {
+        for (var i = 0; i < dt.items.length; i++) {
+            dt.items.remove(i);
+        }
+    } else {
+        event.dataTransfer.clearData();
+    }
+}
