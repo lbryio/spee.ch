d4be277ffa2a32a1a021e9963c8b433b5be21556
bill bittner <bittner.w@gmail.com>
2017-10-17 14:15:02 -0700

    changed /media/ to speech url (#126)

--- helpers/serveHelpers.js
+++ helpers/serveHelpers.js
@@ -5,7 +5,7 @@ function createOpenGraphInfo ({ fileType, claimId, name, fileName, fileExt }) {
     embedUrl     : `https://spee.ch/embed/${claimId}/${name}`,
     showUrl      : `https://spee.ch/${claimId}/${name}`,
     source       : `https://spee.ch/${claimId}/${name}${fileExt}`,
-    directFileUrl: `https://spee.ch/media/${fileName}`,
+    directFileUrl: `https://spee.ch/${claimId}/${name}${fileExt}`,
   };
 }
 
