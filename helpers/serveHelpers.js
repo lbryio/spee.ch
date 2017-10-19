d21cb018cc7512e7e7c3637566c0b93619ba6faf
bill bittner <bittner.w@gmail.com>
2017-10-19 08:49:02 -0700

    updated meta tags

--- helpers/serveHelpers.js
+++ helpers/serveHelpers.js
@@ -4,8 +4,8 @@ function createOpenGraphInfo ({ fileType, claimId, name, fileName, fileExt }) {
   return {
     embedUrl     : `https://spee.ch/embed/${claimId}/${name}`,
     showUrl      : `https://spee.ch/${claimId}/${name}`,
-    source       : `https://spee.ch/${claimId}/${name}${fileExt}`,
-    directFileUrl: `https://spee.ch/${claimId}/${name}${fileExt}`,
+    source       : `https://spee.ch/${claimId}/${name}.${fileExt}`,
+    directFileUrl: `https://spee.ch/${claimId}/${name}.${fileExt}`,
   };
 }
 
