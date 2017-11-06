c881d7c64b6e2bada1b7b3cafb4a5354e867410c
bill bittner <bittner.w@gmail.com>
2017-11-06 15:26:45 -0800

    added comments to speechConfig_example.js

--- config/speechConfig_example.js
+++ config/speechConfig_example.js
@@ -1,23 +1,22 @@
 module.exports = {
   wallet: {
-    lbryClaimAddress: null,
-    defaultChannel  : null,
+    lbryClaimAddress: null,  // choose an address from your lbry wallet
   },
   analytics: {
-    googleId: null,
+    googleId: null, // google id for analytics tracking; leave `null` if not applicable
   },
   sql: {
-    database: 'lbry',
-    username: null,
-    password: null,
+    database: null,  // name of mysql database
+    username: null,  // username for mysql
+    password: null,  // password for mysql
   },
   logging: {
-    logLevel         : null,
-    slackWebHook     : null,
-    slackErrorChannel: null,
-    slackInfoChannel : null,
+    logLevel         : null,  // options: silly, debug, verbose, info
+    slackWebHook     : null,  // enter a webhook if you wish to push logs to slack; otherwise leave as `null`
+    slackErrorChannel: null,  // enter a slack channel (#example) for errors to be sent to; otherwise leave null
+    slackInfoChannel : null,  // enter a slack channel (#info) for info level logs to be sent to otherwise leave null
   },
   session: {
-    sessionKey: null,
+    sessionKey: null,  // enter a secret key to be used for session encryption
   },
 };
