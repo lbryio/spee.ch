912363e936a5b82d3c57051f8d09ea8a1e0b85f4
bill bittner <bittner.w@gmail.com>
2017-11-07 10:57:31 -0800

    added sequelizeCli config example

---
+++ config/sequelizeCliConfig_example.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: 'database_dev',
+    password: 'database_dev',
+    database: 'database_dev',
+    host    : '127.0.0.1',
+    dialect : 'mysql',
+  },
+  test: {
+    username: 'database_dev',
+    password: 'database_dev',
+    database: 'database_dev',
+    host    : '127.0.0.1',
+    dialect : 'mysql',
+  },
+  production: {
+    username: 'database_dev',
+    password: 'database_dev',
+    database: 'database_dev',
+    host    : '127.0.0.1',
+    dialect : 'mysql',
+  },
+};
