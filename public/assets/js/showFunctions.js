e5d3fdf3221d25e2867824f196e77b1cc799586f
bill bittner <bittner.w@gmail.com>
2017-11-29 16:02:40 -0800

    fixed the play/pause functionality on videos

--- public/assets/js/showFunctions.js
+++ public/assets/js/showFunctions.js
@@ -1,23 +1,26 @@
-function playOrPause(video){
-    if (video.paused == true) {
-        video.play();
-    }
-    else{
-        video.pause();
-    }
-}
-
-// if a video player is present, set the listeners
-const video = document.getElementById('video-player');
-if (video) {
-    // add event listener for click
-    video.addEventListener('click', ()=> {
-        playOrPause(video);
-    });
-    // add event listener for space bar
-    document.body.onkeyup = (event) => {
-        if (event.keyCode == 32) {
-            playOrPause(video);
+const showFunctions = {
+    addPlayPauseToVideo: function () {
+        const that = this;
+        const video = document.getElementById('video-asset');
+        if (video) {
+            // add event listener for click
+            video.addEventListener('click', ()=> {
+                that.playOrPause(video);
+            });
+            // add event listener for space bar
+            document.body.onkeyup = (event) => {
+                if (event.keyCode == 32) {
+                    that.playOrPause(video);
+                }
+            };
         }
-    };
+    },
+    playOrPause: function(video){
+        if (video.paused == true) {
+            video.play();
+        }
+        else{
+            video.pause();
+        }
+    }
 }
