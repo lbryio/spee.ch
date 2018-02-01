bfa623ec1741b327c2e8710bc518e5d58b92f285
bill bittner <bittner.w@gmail.com>
2018-01-31 19:21:00 -0800

    removed unnecessary handlebars page routes

--- routes/page-routes.js
+++ routes/page-routes.js
@@ -1,5 +1,3 @@
-const errorHandlers = require('../helpers/errorHandlers.js');
-const { getTrendingClaims, getRecentClaims } = require('../controllers/statsController.js');
 const { site } = require('../config/speechConfig.js');
 
 module.exports = (app) => {
@@ -21,28 +19,11 @@ module.exports = (app) => {
     res.status(301).redirect('/popular');
   });
   app.get('/popular', ({ ip, originalUrl }, res) => {
-    const startDate = new Date();
-    startDate.setDate(startDate.getDate() - 1);
-    const dateTime = startDate.toISOString().slice(0, 19).replace('T', ' ');
-    getTrendingClaims(dateTime)
-      .then(result => {
-        res.status(200).render('popular', {
-          trendingAssets: result,
-        });
-      })
-      .catch(error => {
-        errorHandlers.handleRequestError(originalUrl, ip, error, res);
-      });
+    res.status(200).render('index');
   });
   // route to display a list of the trending images
   app.get('/new', ({ ip, originalUrl }, res) => {
-    getRecentClaims()
-      .then(result => {
-        res.status(200).render('new', { newClaims: result });
-      })
-      .catch(error => {
-        errorHandlers.handleRequestError(originalUrl, ip, error, res);
-      });
+    res.status(200).render('index');
   });
   // route to send embedable video player (for twitter)
   app.get('/embed/:claimId/:name', ({ params }, res) => {
@@ -51,10 +32,5 @@ module.exports = (app) => {
     const host = site.host;
     // get and render the content
     res.status(200).render('embed', { layout: 'embed', host, claimId, name });
-  });
-  // route to display all free public claims at a given name
-  app.get('/:name/all', (req, res) => {
-    // get and render the content
-    res.status(410).send('/:name/all is no longer supported');
   });
 };
