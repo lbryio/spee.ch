e93e6cc5d792f013338bc36e6d2a6993bcc47aee
YULIUS KURNIAWAN KRISTIANTO <yuliuskristianto9@gmail.com>
2018-03-13 00:22:45 +0700

    add download link

--- react/components/AboutPage/index.jsx
+++ react/components/AboutPage/index.jsx
@@ -20,7 +20,7 @@ class AboutPage extends React.Component {
           </div><div className='column column--5 column--med-10 align-content-top'>
             <div className='column column--8 column--med-10'>
               <p>Spee.ch is a media-hosting site that reads from and publishes content to the <a className='link--primary' href='https://lbry.io'>LBRY</a> blockchain.</p>
-              <p>Spee.ch is a hosting service, but with the added benefit that it stores your content on a decentralized network of computers -- the LBRY network.  This means that your images are stored in multiple locations without a single point of failure.</p>
+              <p>Spee.ch is a hosting service, but with the added benefit that it stores your content on a decentralized network of computers -- the <a className='link--primary' href='https://lbry.io/get'>LBRY</a> network.  This means that your images are stored in multiple locations without a single point of failure.</p>
               <h3>Contribute</h3>
               <p>If you have an idea for your own spee.ch-like site on top of LBRY, fork our <a className='link--primary' href='https://github.com/lbryio/spee.ch'>github repo</a> and go to town!</p>
               <p>If you want to improve spee.ch, join our <a className='link--primary' href='https://discord.gg/YjYbwhS'>discord channel</a> or solve one of our <a className='link--primary' href='https://github.com/lbryio/spee.ch/issues'>github issues</a>.</p>
