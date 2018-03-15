0aa78efa5d36cbb12b8f47eb64dab30487038b91
bill bittner <bittner.w@gmail.com>
2018-03-15 15:35:58 -0700

    reverted GA component

--- react/containers/FourOhFourPage/view.jsx
+++ react/containers/FourOhFourPage/view.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavBar from 'containers/NavBar/index';
+import NavBar from 'containers/NavBar';
 import Helmet from 'react-helmet';
 
 class FourOhForPage extends React.Component {
--- react/containers/GAListener/index.jsx
+++ react/containers/GAListener/index.jsx
@@ -1,10 +1,23 @@
-import { connect } from 'react-redux';
-import View from './view';
+import React from 'react';
+import GoogleAnalytics from 'react-ga';
+import { withRouter } from 'react-router-dom';
 
-const mapStateToProps = ({ site: { googleAnalyticsId } }) => {
-  return {
-    googleAnalyticsId,
-  };
-};
+GoogleAnalytics.initialize('test');
 
-export default connect(mapStateToProps, null)(View);
+class GAListener extends React.Component {
+  componentDidMount () {
+    this.sendPageView(this.props.history.location);
+    this.props.history.listen(this.sendPageView);
+  }
+
+  sendPageView (location) {
+    GoogleAnalytics.set({ page: location.pathname });
+    GoogleAnalytics.pageview(location.pathname);
+  }
+
+  render () {
+    return this.props.children;
+  }
+}
+
+export default withRouter(GAListener);
