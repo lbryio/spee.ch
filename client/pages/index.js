fa2080694df432d990fa20a93491b5e01cfc0b7d
bill bittner <bittner.w@gmail.com>
2018-03-30 15:27:28 -0700

    moved build tools from build to tools

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,5 @@
 const Path = require('path');
-const { getSubDirectoryNames } = require('build/getFolderNames.js');
+const { getSubDirectoryNames } = require('tools/getFolderNames.js');
 const thisFolder = Path.resolve(__dirname, 'client/pages/');
 
 let modules = {};
