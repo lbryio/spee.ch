6476121de8b611f9a06ea8ccb6c627665006d288
bill bittner <bittner.w@gmail.com>
2018-04-08 19:54:09 -0400

    removed testing logs and rebuilt

--- server/middleware/requestLogger.js
+++ server/middleware/requestLogger.js
@@ -1,7 +1,7 @@
 const logger = require('winston');
 
 const requestLogger = (req, res, next) => {  // custom logging middleware to log all incoming http requests
-  logger.verbose(`Request on ${req.originalUrl} from ${req.ip}`);
+  logger.debug(`Request on ${req.originalUrl} from ${req.ip}`);
   next();
 };
 
