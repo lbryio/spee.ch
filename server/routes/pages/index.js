dc98b653d71d81aa0fb3b9ee56371c851f4a13b7
bill bittner <bittner.w@gmail.com>
2018-05-09 23:52:43 -0700

    added /multisite route

--- server/routes/pages/index.js
+++ server/routes/pages/index.js
@@ -9,5 +9,6 @@ module.exports = (app) => {
   app.get('/trending', redirect('/popular'));
   app.get('/popular', handlePageRequest);
   app.get('/new', handlePageRequest);
+  app.get('/multisite', handlePageRequest);
   app.get('/embed/:claimId/:name', handleEmbedRequest);  // route to send embedable video player (for twitter)
 };
