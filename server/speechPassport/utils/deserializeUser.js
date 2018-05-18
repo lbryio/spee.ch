07b1d9dabdc3a3027e5a12fbe96a6efb34877c30
bill bittner <bittner.w@gmail.com>
2018-05-18 10:58:16 -0700

    removed serialize/deserialise user console logs

--- server/speechPassport/utils/deserializeUser.js
+++ server/speechPassport/utils/deserializeUser.js
@@ -1,6 +1,5 @@
 const deserializeUser = (user, done) => {
   // deserializes session and populates additional info to req.user
-  console.log('deserializing user');
   done(null, user);
 };
 
--- server/speechPassport/utils/serializeUser.js
+++ server/speechPassport/utils/serializeUser.js
@@ -1,6 +1,5 @@
 const serializeUser = (user, done) => {
   // returns user data to be serialized into session
-  console.log('serializing user');
   done(null, user);
 };
 
