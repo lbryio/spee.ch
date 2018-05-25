b569bfdca6fb6c10c806c3e731f72132f26ab2a3
bill bittner <bittner.w@gmail.com>
2018-05-25 09:30:28 -0700

    Revert "added referrer to serve event analytics"
    This reverts commit 0c9e47546c9bf17d7a3ff8b378b316cf56ac218c.

--- server/utils/googleAnalytics.js
+++ server/utils/googleAnalytics.js
@@ -9,9 +9,8 @@ function createServeEventParams (headers, ip, originalUrl) {
     eventLabel       : originalUrl,
     ipOverride       : ip,
     userAgentOverride: headers['user-agent'],
-    documentReferrer : headers['referer'],
   };
-}
+};
 
 function createPublishTimingEventParams (category, variable, label, startTime, endTime) {
   const duration = endTime - startTime;
@@ -21,7 +20,7 @@ function createPublishTimingEventParams (category, variable, label, startTime, e
     userTimingTime        : duration,
     userTimingLabel       : label,
   };
-}
+};
 
 function sendGoogleAnalyticsEvent (ip, params) {
   const visitorId = ip.replace(/\./g, '-');
@@ -30,9 +29,8 @@ function sendGoogleAnalyticsEvent (ip, params) {
     if (err) {
       logger.error('Google Analytics Event Error >>', err);
     }
-    logger.debug(`Event successfully sent to google analytics`);
   });
-}
+};
 
 function sendGoogleAnalyticsTiming (visitorId, params) {
   const visitor = ua(googleId, visitorId, { strictCidFormat: false, https: true });
@@ -42,25 +40,18 @@ function sendGoogleAnalyticsTiming (visitorId, params) {
     }
     logger.debug(`Timing event successfully sent to google analytics`);
   });
-}
-
-function sendGAServeEvent (headers, ip, originalUrl) {
-  logger.debug('headers:', headers);
-  const params = createServeEventParams(headers, ip, originalUrl);
-  sendGoogleAnalyticsEvent(ip, params);
-}
-
-function sendGATimingEvent (category, variable, label, startTime, endTime) {
-  const params = createPublishTimingEventParams(category, variable, label, startTime, endTime);
-  sendGoogleAnalyticsTiming(title, params);
-}
-
-function chooseGaLbrynetPublishLabel ({ channel_name: channelName, channel_id: channelId }) {
-  return (channelName || channelId ? 'PUBLISH_IN_CHANNEL_CLAIM' : 'PUBLISH_ANONYMOUS_CLAIM');
-}
+};
 
 module.exports = {
-  sendGAServeEvent,
-  sendGATimingEvent,
-  chooseGaLbrynetPublishLabel,
+  sendGAServeEvent (headers, ip, originalUrl) {
+    const params = createServeEventParams(headers, ip, originalUrl);
+    sendGoogleAnalyticsEvent(ip, params);
+  },
+  sendGATimingEvent (category, variable, label, startTime, endTime) {
+    const params = createPublishTimingEventParams(category, variable, label, startTime, endTime);
+    sendGoogleAnalyticsTiming(title, params);
+  },
+  chooseGaLbrynetPublishLabel ({ channel_name: channelName, channel_id: channelId }) {
+    return (channelName || channelId ? 'PUBLISH_IN_CHANNEL_CLAIM' : 'PUBLISH_ANONYMOUS_CLAIM');
+  },
 };
