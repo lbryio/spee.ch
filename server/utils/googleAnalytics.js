3f5b20f5db285113d9f745939cff1e11237d29aa
bill bittner <bittner.w@gmail.com>
2018-06-05 16:24:27 -0700

    changed functions to consts

--- server/utils/googleAnalytics.js
+++ server/utils/googleAnalytics.js
@@ -2,7 +2,7 @@ const logger = require('winston');
 const ua = require('universal-analytics');
 const { analytics : { googleId }, details: { title } } = require('../../config/siteConfig.js');
 
-function createServeEventParams (headers, ip, originalUrl) {
+const createServeEventParams = (headers, ip, originalUrl) => {
   return {
     eventCategory    : 'client requests',
     eventAction      : 'serve request',
@@ -11,9 +11,9 @@ function createServeEventParams (headers, ip, originalUrl) {
     userAgentOverride: headers['user-agent'],
     documentReferrer : headers['referer'],
   };
-}
+};
 
-function createPublishTimingEventParams (category, variable, label, startTime, endTime) {
+const createPublishTimingEventParams = (category, variable, label, startTime, endTime) => {
   const duration = endTime - startTime;
   return {
     userTimingCategory    : category,
@@ -21,43 +21,42 @@ function createPublishTimingEventParams (category, variable, label, startTime, e
     userTimingTime        : duration,
     userTimingLabel       : label,
   };
-}
+};
 
-function sendGoogleAnalyticsEvent (ip, params) {
+const sendGoogleAnalyticsEvent = (ip, params) => {
   const visitorId = ip.replace(/\./g, '-');
   const visitor = ua(googleId, visitorId, { strictCidFormat: false, https: true });
   visitor.event(params, (err) => {
     if (err) {
-      logger.error('Google Analytics Event Error >>', err);
+      return logger.error('Google Analytics Event Error >>', err);
     }
     logger.debug(`Event successfully sent to google analytics`);
   });
-}
+};
 
-function sendGoogleAnalyticsTiming (visitorId, params) {
+const sendGoogleAnalyticsTiming = (visitorId, params) => {
   const visitor = ua(googleId, visitorId, { strictCidFormat: false, https: true });
   visitor.timing(params, (err) => {
     if (err) {
-      logger.error('Google Analytics Event Error >>', err);
+      return logger.error('Google Analytics Event Error >>', err);
     }
     logger.debug(`Timing event successfully sent to google analytics`);
   });
-}
+};
 
-function sendGAServeEvent (headers, ip, originalUrl) {
-  logger.debug('headers:', headers);
+const sendGAServeEvent = (headers, ip, originalUrl) => {
   const params = createServeEventParams(headers, ip, originalUrl);
   sendGoogleAnalyticsEvent(ip, params);
-}
+};
 
-function sendGATimingEvent (category, variable, label, startTime, endTime) {
+const sendGATimingEvent = (category, variable, label, startTime, endTime) => {
   const params = createPublishTimingEventParams(category, variable, label, startTime, endTime);
   sendGoogleAnalyticsTiming(title, params);
-}
+};
 
-function chooseGaLbrynetPublishLabel ({ channel_name: channelName, channel_id: channelId }) {
+const chooseGaLbrynetPublishLabel = ({ channel_name: channelName, channel_id: channelId }) => {
   return (channelName || channelId ? 'PUBLISH_IN_CHANNEL_CLAIM' : 'PUBLISH_ANONYMOUS_CLAIM');
-}
+};
 
 module.exports = {
   sendGAServeEvent,
