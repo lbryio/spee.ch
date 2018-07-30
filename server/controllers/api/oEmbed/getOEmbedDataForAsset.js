b401919bb41a7232d7dd7c9fa0bd3f4673b412cd
bill bittner <bittner.w@gmail.com>
2018-07-30 15:23:26 -0700

    updated serve url

--- server/controllers/api/oEmbed/getOEmbedDataForAsset.js
+++ server/controllers/api/oEmbed/getOEmbedDataForAsset.js
@@ -5,9 +5,7 @@ const getClaimId = require('../../utils/getClaimId');
 const getOEmbedDataForAsset = (channelName, channelClaimId, claimName, claimId) => {
   let fileData, claimData;
   let data = {
-    version      : 1.0,
-    author_name  : 'Spee.ch',
-    author_url   : 'https://spee.ch',
+    version      : '1.0',
     provider_name: 'Spee.ch',
     provider_url : 'https://spee.ch',
     cache_age    : 86400, // one day in seconds
@@ -38,17 +36,21 @@ const getOEmbedDataForAsset = (channelName, channelClaimId, claimName, claimId) 
     .then(fileRecord => {
       fileData = fileRecord.dataValues;
       logger.debug('file data:', fileData);
+      const serveUrl = `https://dev1.spee.ch/${fileData.claimId}/${fileData.name}.${fileData.fileType.substring(fileData.fileType.indexOf('/') + 1)}`;
       // set the resource type
       if (fileData.fileType === 'video/mp4') {
         data['type'] = 'video';
+        data['html'] = `<video width="100%" controls poster="${claimData.thumbnail}" src="${serveUrl}"/></video>`;
       } else {
         data['type'] = 'picture';
+        data['url'] = serveUrl;
       }
       // get the data
       data['title'] = claimData.title;
-      data['thumbnail_url'] = `https://dev1.spee.ch/${fileData.claimId}/${fileData.name}.${fileData.fileType.substring('/')}`;
-      data['thumbnail_width'] = fileData.width || 600;
-      data['thumbnail_height'] = fileData.height || 400;
+      data['width'] = fileData.width || 600;
+      data['height'] = fileData.height || 400;
+      data['author_name'] = 'Spee.ch';
+      data['author_url'] = 'https://spee.ch';
     })
     .then(() => {
       return data;
