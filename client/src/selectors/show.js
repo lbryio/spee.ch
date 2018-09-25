bd1424acffe94880db5577836348d8fc77275638
Travis Eden <daovist@gmail.com>
2018-09-25 09:53:45 -0400

    fix selectAsset

--- client/src/selectors/show.js
+++ client/src/selectors/show.js
@@ -1,7 +1,13 @@
-export const selectAsset = (show) => {
-  const request = show.requestList[show.request.id];
-  const assetKey = request.key;
-  return show.assetList[assetKey];
+export const selectAsset = show => {
+  const requestId = show.request.id;
+  let asset;
+  const request = show.requestList[requestId] || null;
+  const assetList = show.assetList;
+  if (request && assetList) {
+    const assetKey = request.key;  // note: just store this in the request
+    asset = assetList[assetKey] || null;
+  }
+  return asset;
 };
 
 export const selectShowState = (state) => {
