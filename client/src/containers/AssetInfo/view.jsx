e0b15691b9a11b3d35c4baa2ee5a93bc882a6aeb
Minesh Mitha <minesh@Mineshs-MacBook-Pro.local>
2018-10-06 18:57:11 +0100

    Forgot to stage the rest of my code, thanks VS Code.

--- client/src/containers/AssetInfo/view.jsx
+++ client/src/containers/AssetInfo/view.jsx
@@ -12,7 +12,7 @@ class AssetInfo extends React.Component {
   render () {
     const { asset: { shortId, claimData : { channelName, certificateId, description, name, claimId, fileExt, contentType, thumbnail, host } } } = this.props;
     return (
-      <div className='asset-information'>
+      <div className='asset-information-wrap'>
         <HorizontalSplit
           leftSide={
             <div className='asset-description'>
@@ -21,106 +21,110 @@ class AssetInfo extends React.Component {
               )}
             </div>
           }
-          rightSide={''}
+          rightSide={
+            <div className='asset-information'>
+              {channelName && (
+                <Row>
+                  <RowLabeled
+                    label={
+                      <Label value={'Channel'} />
+                    }
+                    content={
+                      <span className='text'>
+                        <Link className='link--brand' to={`/${channelName}:${certificateId}`}>{channelName}</Link>
+                      </span>
+                    }
+                  />
+                </Row>
+              )}
+
+              <Row>
+                <RowLabeled
+                  label={
+                    <Label value={'Share'} />
+                  }
+                  content={
+                    <AssetShareButtons
+                      host={host}
+                      name={name}
+                      shortId={shortId}
+                    />
+                  }
+                />
+              </Row>
+
+              <Row>
+                <RowLabeled
+                  label={
+                    <Label value={'Link'} />
+                  }
+                  content={
+                    <ClickToCopy
+                      id={'short-link'}
+                      value={`${host}/${shortId}/${name}`}
+                    />
+                  }
+                />
+              </Row>
+
+              <Row>
+                <RowLabeled
+                  label={
+                    <Label value={'Embed'} />
+                  }
+                  content={
+                    <div>
+                      {(contentType === 'video/mp4') ? (
+                        <ClickToCopy
+                          id={'embed-text-video'}
+                          value={`<video width="100%" controls poster="${thumbnail}" src="${host}/${claimId}/${name}.${fileExt}"/></video>`}
+                        />
+                      ) : (
+                        <ClickToCopy
+                          id={'embed-text-image'}
+                          value={`<img src="${host}/${claimId}/${name}.${fileExt}"/>`}
+                        />
+                      )}
+                    </div>
+                  }
+                />
+              </Row>
+
+              <Row>
+                <SpaceBetween>
+                  <a
+                    className='link--brand'
+                    href={`${host}/${claimId}/${name}.${fileExt}`}
+                  >
+                    Direct Link
+                  </a>
+                  <a
+                    className={'link--brand'}
+                    href={`${host}/${claimId}/${name}.${fileExt}`}
+                    download={name}
+                  >
+                    Download
+                  </a>
+                  <a
+                    className={'link--brand'}
+                    target='_blank'
+                    href='https://lbry.io/dmca'
+                  >
+                    Report
+                  </a>
+                </SpaceBetween>
+              </Row>
+
+              <Row>
+                <p>
+                  Hosted via the <a className={'link--brand'} href={'https://lbry.io/get'} target={'_blank'}>LBRY</a> blockchain
+                </p>
+              </Row>
+
+            </div>
+          }
         />
 
-        {channelName && (
-          <Row>
-            <RowLabeled
-              label={
-                <Label value={'Channel:'} />
-              }
-              content={
-                <span className='text'>
-                  <Link to={`/${channelName}:${certificateId}`}>{channelName}</Link>
-                </span>
-              }
-            />
-          </Row>
-        )}
-
-        <Row>
-          <RowLabeled
-            label={
-              <Label value={'Share:'} />
-            }
-            content={
-              <AssetShareButtons
-                host={host}
-                name={name}
-                shortId={shortId}
-              />
-            }
-          />
-        </Row>
-
-        <Row>
-          <RowLabeled
-            label={
-              <Label value={'Link:'} />
-            }
-            content={
-              <ClickToCopy
-                id={'short-link'}
-                value={`${host}/${shortId}/${name}`}
-              />
-            }
-          />
-        </Row>
-
-        <Row>
-          <RowLabeled
-            label={
-              <Label value={'Embed:'} />
-            }
-            content={
-              <div>
-                {(contentType === 'video/mp4') ? (
-                  <ClickToCopy
-                    id={'embed-text-video'}
-                    value={`<video width="100%" controls poster="${thumbnail}" src="${host}/${claimId}/${name}.${fileExt}"/></video>`}
-                  />
-                ) : (
-                  <ClickToCopy
-                    id={'embed-text-image'}
-                    value={`<img src="${host}/${claimId}/${name}.${fileExt}"/>`}
-                  />
-                )}
-              </div>
-            }
-          />
-        </Row>
-
-        <Row>
-          <SpaceBetween>
-            <a
-              className='link--primary'
-              href={`${host}/${claimId}/${name}.${fileExt}`}
-            >
-              Direct Link
-            </a>
-            <a
-              className={'link--primary'}
-              href={`${host}/${claimId}/${name}.${fileExt}`}
-              download={name}
-            >
-              Download
-            </a>
-            <a
-              className={'link--primary'}
-              target='_blank'
-              href='https://lbry.io/dmca'
-            >
-              Report
-            </a>
-          </SpaceBetween>
-        </Row>
-
-        <Row>
-          <p>
-            Hosted via the <a className={'link--primary'} href={'https://lbry.io/get'} target={'_blank'}>LBRY</a> blockchain
-          </p>
-        </Row>
 
       </div>
     );
