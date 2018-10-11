8759bcfdd6e7925a03b19902f58d01c98e1172c8
Minesh Mitha <minesh@Mineshs-MacBook-Pro.local>
2018-10-11 21:51:33 +0100

    New sharing icons

--- client/src/components/AssetShareButtons/index.js
+++ client/src/components/AssetShareButtons/index.js
@@ -1,38 +1,37 @@
 import React from 'react';
-import SpaceBetween from '@components/SpaceBetween';
 
 const AssetShareButtons = ({ host, name, shortId }) => {
   return (
-    <SpaceBetween >
+    <div className='share-buttons'>
       <a
         className='link--primary'
         target='_blank'
         href={`https://twitter.com/intent/tweet?text=${host}/${shortId}/${name}`}
       >
-        twitter
+        <img src='/assets/img/icn_twitter.svg' />
       </a>
       <a
         className='link--primary'
         target='_blank'
         href={`https://www.facebook.com/sharer/sharer.php?u=${host}/${shortId}/${name}`}
       >
-        facebook
+        <img src='/assets/img/icn_facebook.svg' />
       </a>
       <a
         className='link--primary'
         target='_blank'
         href={`http://tumblr.com/widgets/share/tool?canonicalUrl=${host}/${shortId}/${name}`}
       >
-        tumblr
+        <img src='/assets/img/icn_tumblr.svg' />
       </a>
       <a
         className='link--primary'
         target='_blank'
         href={`https://www.reddit.com/submit?url=${host}/${shortId}/${name}&title=${name}`}
       >
-        reddit
+        <img src='/assets/img/icn_reddit.svg' />
       </a>
-    </SpaceBetween>
+    </div>
   );
 };
 
