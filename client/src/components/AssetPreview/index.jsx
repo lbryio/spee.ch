b87a76880321ef4ef7b97e84177f4abe96160693
Travis Eden <daovist@gmail.com>
2018-10-11 13:03:39 -0400

    show title on asset previews

--- client/src/components/AssetPreview/index.jsx
+++ client/src/components/AssetPreview/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const AssetPreview = ({ defaultThumbnail, claimData: { name, claimId, fileExt, contentType, thumbnail } }) => {
+const AssetPreview = ({ defaultThumbnail, claimData: { name, claimId, fileExt, contentType, thumbnail, title } }) => {
   const embedUrl = `/${claimId}/${name}.${fileExt}`;
   const showUrl = `/${claimId}/${name}`;
   return (
@@ -13,19 +13,25 @@ const AssetPreview = ({ defaultThumbnail, claimData: { name, claimId, fileExt, c
           case 'image/png':
           case 'image/gif':
             return (
-              <img
-                className={'asset-preview-image'}
-                src={embedUrl}
-                alt={name}
-              />
+              <div>
+                <h6 class='list-title'>{title}</h6>
+                <img
+                  className={'asset-preview-image'}
+                  src={embedUrl}
+                  alt={name}
+                />
+              </div>
             );
           case 'video/mp4':
             return (
-              <img
-                className={'asset-preview-video'}
-                src={thumbnail || defaultThumbnail}
-                alt={name}
-              />
+              <div>
+                <h6 class='list-title'>{title}</h6>
+                <img
+                  className={'asset-preview-video'}
+                  src={thumbnail || defaultThumbnail}
+                  alt={name}
+                />
+              </div>
             );
           default:
             return (
