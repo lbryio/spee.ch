de27ae81a86aa4257df1331ffa099d071756141c
Minesh Mitha <minesh@Mineshs-MacBook-Pro.local>
2018-10-14 19:42:47 +0100

    Added border left on asset information + variable change on collapse component

--- client/src/components/VerticalCollapsibleSplit/index.jsx
+++ client/src/components/VerticalCollapsibleSplit/index.jsx
@@ -5,24 +5,24 @@ class VerticalCollapsibleSplit extends React.Component {
   constructor (props) {
     super(props);
     this.collapse = this.collapse.bind(this);
-    this.state = { open: true };
+    this.state = { closed: false };
   }
 
   collapse () {
-    this.setState({ open: !this.state.open });
-    document.getElementById(this.props.name).classList.toggle('hidden');
+    this.setState({ closed: !this.state.closed });
+    document.querySelectorAll(`[data-name='${this.props.name}']`).forEach(el => el.classList.toggle('closed'));
   }
 
   render () {
     return (
       <div className={'vertical-split'}>
-        <div className='visible-content'>
+        <div className='visible-content' data-name={this.props.name}>
           {this.props.top}
           <button className='collapse-button' onClick={this.collapse}>
-            {this.state.open ? <Icon.PlusSquare /> : <Icon.MinusSquare /> }
+            {this.state.closed ? <Icon.MinusSquare /> : <Icon.PlusSquare /> }
           </button>
         </div>
-        <div className='collapse-content' id={this.props.name}>
+        <div className='collapse-content' data-name={this.props.name}>
           {this.props.bottom}
         </div>
       </div>
