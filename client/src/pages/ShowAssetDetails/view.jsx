be2d3cb3da2e03ff343b68b5583933e91a243e3d
Minesh Mitha <minesh@mineshs-MBP.local>
2018-10-25 20:07:41 +0100

    Removed unused components

--- client/src/pages/ShowAssetDetails/view.jsx
+++ client/src/pages/ShowAssetDetails/view.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import PageLayout from '@components/PageLayout';
 
 import VerticalCollapsibleSplit from '@components/VerticalCollapsibleSplit';
-// import VerticalSplit from '@components/VerticalSplit';
-// import AssetTitle from '@containers/AssetTitle';
 import AssetDisplay from '@containers/AssetDisplay';
 import AssetInfo from '@containers/AssetInfo';
 import ErrorPage from '@pages/ErrorPage';
