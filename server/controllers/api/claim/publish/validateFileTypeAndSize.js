5ee553bfc22850f49c702507da372c4432c71207
Shawn <shawn@kafei.io>
2018-11-27 23:20:48 -0600

    Add upload limit configurations

--- server/controllers/api/claim/publish/validateFileTypeAndSize.js
+++ server/controllers/api/claim/publish/validateFileTypeAndSize.js
@@ -1,26 +1,36 @@
 const logger = require('winston');
 
+const {
+  publishing: {
+    maxSizeImage = 10000000,
+    maxSizeGif = 50000000,
+    maxSizeVideo = 50000000,
+  }
+} = require('@config/siteConfig');
+
+const SIZE_MB = 1000000;
+
 const validateFileTypeAndSize = (file) => {
   // check file type and size
   switch (file.type) {
     case 'image/jpeg':
     case 'image/jpg':
     case 'image/png':
-      if (file.size > 10000000) {
+      if (file.size > maxSizeImage) {
         logger.debug('publish > file validation > .jpeg/.jpg/.png was too big');
-        throw new Error('Sorry, images are limited to 10 megabytes.');
+        throw new Error(`Sorry, images are limited to ${maxSizeImage / SIZE_MB} megabytes.`);
       }
       break;
     case 'image/gif':
-      if (file.size > 50000000) {
+      if (file.size > maxSizeGif) {
         logger.debug('publish > file validation > .gif was too big');
-        throw new Error('Sorry, .gifs are limited to 50 megabytes.');
+        throw new Error(`Sorry, .gifs are limited to ${maxSizeGif / SIZE_MB} megabytes.`);
       }
       break;
     case 'video/mp4':
-      if (file.size > 50000000) {
+      if (file.size > maxSizeVideo) {
         logger.debug('publish > file validation > .mp4 was too big');
-        throw new Error('Sorry, videos are limited to 50 megabytes.');
+        throw new Error(`Sorry, videos are limited to ${maxSizeVideo / SIZE_MB} megabytes.`);
       }
       break;
     default:
