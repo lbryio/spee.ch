0a65906130c5a753e999368d955280fe78224b45
jessop <jessopb@gmail.com>
2018-11-29 17:01:01 -0500

    ensures that getClaimIdByLongChannelId tests for bidstate

--- server/chainquery/queries/claimQueries.js
+++ server/chainquery/queries/claimQueries.js
@@ -116,7 +116,7 @@ export default (db, table, sequelize) => ({
   getClaimIdByLongChannelId: async (channelClaimId, claimName) => {
     logger.debug(`finding claim id for claim ${claimName} from channel ${channelClaimId}`);
     return await table.findAll({
-      where: { name: claimName, publisher_id: channelClaimId },
+      where: { name: claimName, publisher_id: channelClaimId, bid_state: { [sequelize.Op.or]: ['Controlling', 'Active', 'Accepted'] } },
       order: [['id', 'ASC']],
     })
     .then(result => {
