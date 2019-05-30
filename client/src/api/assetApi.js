dee61b5b6f98f4c02764d756b15a7184ff8152b6
Tom <thomas.zarebczan@gmail.com>
2019-05-30 14:57:47 -0400

    fix: abandoning claims
    Confirmed working locally. File/claims table has outpoint. Claim was abandoned by the sdk.

--- client/src/api/assetApi.js
+++ client/src/api/assetApi.js
@@ -1,6 +1,6 @@
 import Request from '../utils/request';
 
-export function getLongClaimId (host, name, modifier) {
+export function getLongClaimId(host, name, modifier) {
   let body = {};
   // create request params
   if (modifier) {
@@ -13,40 +13,40 @@ export function getLongClaimId (host, name, modifier) {
   }
   body['claimName'] = name;
   const params = {
-    method : 'POST',
+    method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body   : JSON.stringify(body),
+    body: JSON.stringify(body),
   };
   // create url
   const url = `${host}/api/claim/long-id`;
   // return the request promise
   return Request(url, params);
-};
+}
 
-export function getShortId (host, name, claimId) {
+export function getShortId(host, name, claimId) {
   const url = `${host}/api/claim/short-id/${claimId}/${name}`;
   return Request(url);
-};
+}
 
-export function getClaimData (host, name, claimId) {
+export function getClaimData(host, name, claimId) {
   const url = `${host}/api/claim/data/${name}/${claimId}`;
   return Request(url);
-};
+}
 
-export function checkClaimAvailability (claim) {
+export function checkClaimAvailability(claim) {
   const url = `/api/claim/availability/${claim}`;
   return Request(url);
 }
 
-export function getClaimViews (claimId) {
+export function getClaimViews(claimId) {
   const url = `/api/claim/views/${claimId}`;
   return Request(url);
 }
 
-export function doAbandonClaim (claimId) {
+export function doAbandonClaim(outpoint) {
   const params = {
-    method : 'POST',
-    body   : JSON.stringify({claimId}),
+    method: 'POST',
+    body: JSON.stringify({ outpoint }),
     headers: new Headers({
       'Content-Type': 'application/json',
     }),
